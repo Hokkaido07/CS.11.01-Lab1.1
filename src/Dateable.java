38c766e21614049046e06606848efd17207b3f69
Hokkaido <alexisc2026@student.cis.edu.hk>
2023-09-07 22:49:02 +0800

    Initial Commit.

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        // defining variables
+        int age;
+        int dateableAge;
+        Scanner scanner = new Scanner(System.in);
+        //Asks user for input of age
+        System.out.println("Please Enter Your Age:");
+         age = scanner.nextInt();
+         dateableAge = 7 + (age/2);
+         // Prints out results based on user input
+        System.out.println("At "+ age + " you can date somebody who is at least " + dateableAge + " years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        int age;
+        int pages;
+        Scanner scanner = new Scanner(System.in);
+        //asks user for age
+        System.out.println("Please enter your age: ");
+        age = scanner.nextInt();
+        pages = 100 - age;
+        // Prints results
+        System.out.println("At the age of " + age + ", you should read " + pages + " pages before giving up on a book.");
+
 
     }
 }
