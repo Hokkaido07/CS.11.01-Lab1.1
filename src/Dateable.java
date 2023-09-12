5c375c5d37ca36210e72847994d621ec93f7aac0
Hokkaido <alexisc2026@student.cis.edu.hk>
2023-09-12 22:07:35 +0800

    Fixed line spacing for lines 19-21 in "dateable" class.

--- src/Dateable.java
+++ src/Dateable.java
@@ -16,9 +16,9 @@ public class Dateable {
         Scanner scanner = new Scanner(System.in);
         //Asks user for input of age
         System.out.println("Please Enter Your Age:");
-         age = scanner.nextInt();
-         dateableAge = 7 + (age/2);
-         // Prints out results based on user input
+        age = scanner.nextInt();
+        dateableAge = 7 + (age/2);
+        // Prints out results based on user input
         System.out.println("At "+ age + " you can date somebody who is at least " + dateableAge + " years old.");
     }
 }
